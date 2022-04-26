fd0deb9093bb6ec2e8237ad785d63ba29edd7c83
Parth Patel <Parth.patel.r@gmail.com>
2022-04-26 08:56:15 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,3 @@
-exports.DATABASE_URL =
-  process.env.DATABASE_URL 
-  global.DATABASE_URL 
+exports.DATABASE_URL =  process.env.DATABASE_URL ||  global.DATABASE_URL 
 exports.options=process.env.OPTIONS 
 exports.PORT = process.env.PORT
